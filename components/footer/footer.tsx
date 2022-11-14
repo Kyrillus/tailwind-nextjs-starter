182adc8e911874c512196895c3f109e3a46cc127
Kyrillus Mehanni <99624083+Kyrillus@users.noreply.github.com>
2022-11-14 16:01:03 +0100

    Feature/footer (#9)
    * add simple footer
    
    Co-Authored-By: STI-Development <117817817+STI-Development@users.noreply.github.com>
    
    * update footer with dynamic current year
    
    Co-Authored-By: STI-Development <117817817+STI-Development@users.noreply.github.com>
    
    Co-authored-by: STI-Development <117817817+STI-Development@users.noreply.github.com>

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Image from "next/image";
 
 function Footer() {
+    const currentYear = new Date().getFullYear();
     return (
         <div>
             <footer className="flex flex-row mt-auto h-24 w-full items-center justify-center border-t">
                 <p className="text-gray-500">
-                    © 2019 by STI GmbH. All Rights Reserved. {' '}
+                    © {currentYear} by STI GmbH. All Rights Reserved.
                 </p>
             </footer>
         </div>
