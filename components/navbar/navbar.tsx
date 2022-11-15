b36f404abb637b5fdd33074d4e57522c8d959006
Kyrillus Mehanni <99624083+Kyrillus@users.noreply.github.com>
2022-11-15 11:08:54 +0100

    update navbar (#10)
    * update navbar
    
    * add hamburger
    * add hamburger functionality
    * change css on menu
    
    Co-Authored-By: STI-Development <117817817+STI-Development@users.noreply.github.com>
    
    * add notifications and outOfFocus menu events
    
    Co-authored-by: STI-Development <117817817+STI-Development@users.noreply.github.com>

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,9 +1,17 @@
 import React, {useState} from 'react';
+import {Fade as Hamburger} from 'hamburger-react'
+import Notification from "./notification";
 
 function Navbar() {
     const navItems = ["Dashboard", "Team", "Projects", "Calendar"];
+    const settingsItems = ["Your Profile", "Settings", "Sign Out"];
     const [profileClicked, setProfileClicked] = useState(false);
-    const toggleProfile = () => {setProfileClicked(!profileClicked)};
+    const [burgerMenuClicked, setburgerMenuClicked] = useState(false);
+    const [notificationBellClicked, setNotificationBellClicked] = useState(false);
+    const toggleBurgerMenu = () => setburgerMenuClicked(!burgerMenuClicked);
+    const toggleProfile = () => setProfileClicked(!profileClicked);
+    const toggleNotificationBellClicked = () => setNotificationBellClicked(!notificationBellClicked);
+
     return (
         <div>
             <nav className="bg-stiBlue shadow">
@@ -12,35 +20,28 @@ function Navbar() {
                         <div className="lg:hidden"/>
                         <div className="flex px-2 lg:px-0">
                             <div className="flex flex-shrink-0 items-center">
-                                <img className="h-8 w-auto lg:h-12"
-                                     src="https://www.sti-consulting.de/wp-content/uploads/2018/11/STI-Consulting_Logo-Straight_White_Padding.png"
+                                <img className="h-8 w-auto lg:h-12 cursor-pointer"
+                                     src="/sti-consulting-logo.png"
                                      alt=""/>
                             </div>
                             <div className="hidden lg:ml-6 lg:flex">
                                 {navItems.map(item => <a key={item}
-                                    className="ml-8 inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out hover:border-gray-300 focus:border-white focus:outline-none"
-                                    href="#">{item}</a>)}
+                                                         className="ml-8 inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-base font-normal leading-5 text-white transition duration-150 ease-in-out hover:border-gray-300"
+                                                         href="#">{item}</a>)}
                             </div>
                         </div>
                         {/* Burger Menu (hidden on lg and upwards) */}
-                        <div className="flex items-center lg:hidden">
-                            <button
-                                className="inline-flex items-center justify-center rounded-md p-2 text-white transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none"
-                                aria-label="Main menu" aria-expanded="false">
-                                <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                          d="M4 6h16M4 12h16M4 18h16"/>
-                                </svg>
-                                <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                          d="M6 18L18 6M6 6l12 12"/>
-                                </svg>
-                            </button>
+                        <div onClick={toggleBurgerMenu} className="flex items-center lg:hidden">
+                            <div className="text-white">
+                                <Hamburger/>
+                            </div>
                         </div>
-                        {/* Notifcation Bell (hidden on small) */}
                         <div className="hidden lg:ml-4 lg:flex lg:items-center">
+                            {/* Notification Bell */}
                             <button
-                                className="flex-shrink-0 rounded-full border-2 border-transparent p-1 text-white transition duration-150 ease-in-out hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none"
+                                onBlur={() => setNotificationBellClicked(false)}
+                                onClick={toggleNotificationBellClicked}
+                                className={"flex-shrink-0 rounded-full border-2 border-transparent p-1 hover:text-gray-400 " + (notificationBellClicked ? "bg-gray-100 text-gray-500" : "bg-transparent text-gray-300")}
                                 aria-label="Notifications">
                                 <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
@@ -50,38 +51,52 @@ function Navbar() {
                             <div className="relative ml-4 flex-shrink-0">
                                 <div>
                                     <button
+                                        onBlur={ () => setProfileClicked(false)}
                                         onClick={toggleProfile}
-                                        className="flex rounded-full border-2 border-transparent text-sm transition duration-150 ease-in-out focus:border-gray-300 focus:outline-none"
+                                        className={"flex rounded-full border-2 text-sm focus:outline-none " + (profileClicked ? "border-gray-100" : "border-transparent")}
                                         id="user-menu" aria-label="User menu" aria-haspopup="true">
                                         <img className="h-8 w-8 rounded-full"
                                              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
                                              alt=""/>
                                     </button>
                                 </div>
-                                <div className={"absolute right-0 mt-2 z-10 w-48 origin-top-right rounded-md shadow-lg " + (profileClicked ? 'block' : 'hidden')}>
+                                {/* Notification Menu */}
+                                <div
+                                    className={"absolute right-12 mt-2 z-10 w-96 origin-top-right rounded-md shadow-lg " + (notificationBellClicked ? 'block' : 'hidden')}>
                                     <div className="shadow-xs rounded-md bg-white py-1" role="menu"
-                                         aria-orientation="vertical" aria-labelledby="user-menu"><a
-                                        className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none"
-                                        href="#" role="menuitem">Your Profile</a><a
-                                        className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none"
-                                        href="#" role="menuitem">Settings</a><a
-                                        className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none"
-                                        href="#" role="menuitem">Sign out</a></div>
+                                         aria-orientation="vertical" aria-labelledby="user-menu">
+                                        <Notification name="Emilia Gates" description="Neue Bewerbung hinzugefügt!" imgsrc="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2.2&amp;w=160&amp;h=160&amp;q=80"/>
+                                        <Notification name="Francis Dera" description="Neue Bewerbung hinzugefügt!" imgsrc="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8;auto=format&amp;fit=facearea&amp;facepad=2.2&amp;w=160&amp;h=160&amp;q=80"/>
+                                    </div>
+                                </div>
+                                {/* Settings Menu */}
+                                <div
+                                    className={"absolute right-0 mt-2 z-10 w-48 origin-top-right rounded-md shadow-lg " + (profileClicked ? 'block' : 'hidden')}>
+                                    <div className="shadow-xs rounded-md bg-white py-1" role="menu"
+                                         aria-orientation="vertical" aria-labelledby="user-menu">
+                                        {settingsItems.map(item =>
+                                            <a
+                                                key={item}
+                                                className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none"
+                                                href="#" role="menuitem"> {item}
+                                            </a>
+                                        )}
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="lg:hidden">
-                    <div className="pt-2 pb-3"><a
-                        className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 transition duration-150 ease-in-out focus:border-indigo-700 focus:bg-indigo-100 focus:text-indigo-800 focus:outline-none"
-                        href="#">Dashboard</a><a
-                        className="mt-1 block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 transition duration-150 ease-in-out hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 focus:border-gray-300 focus:bg-gray-50 focus:text-gray-800 focus:outline-none"
-                        href="#">Team</a><a
-                        className="mt-1 block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 transition duration-150 ease-in-out hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 focus:border-gray-300 focus:bg-gray-50 focus:text-gray-800 focus:outline-none"
-                        href="#">Projects</a><a
-                        className="mt-1 block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 transition duration-150 ease-in-out hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 focus:border-gray-300 focus:bg-gray-50 focus:text-gray-800 focus:outline-none"
-                        href="#">Calendar</a></div>
+                <div className={"lg:hidden  " + (burgerMenuClicked ? 'block' : 'hidden')}>
+                    <div className="pt-2 pb-3">
+                        {navItems.map(item => <a
+                            key={item}
+                            className="block py-2 pl-3 pr-4 text-base font-medium text-white transition duration-150 ease-in-out"
+                            href="#">{item}</a>
+                        )}
+
+                    </div>
                     <div className="border-t border-gray-200 pt-4 pb-3">
                         <div className="flex items-center px-4">
                             <div className="flex-shrink-0">
@@ -90,17 +105,19 @@ function Navbar() {
                                      alt=""/>
                             </div>
                             <div className="ml-3">
-                                <div className="text-base font-medium leading-6 text-gray-800">Tom Cook</div>
-                                <div className="text-sm font-medium leading-5 text-gray-500">tom@example.com</div>
+                                <div className="text-base font-medium leading-6 text-white">Tom Cook</div>
+                                <div className="text-sm font-medium leading-5 text-gray-300">tom@example.com</div>
                             </div>
                         </div>
-                        <div className="mt-3"><a
-                            className="block px-4 py-2 text-base font-medium text-gray-500 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-800 focus:bg-gray-100 focus:text-gray-800 focus:outline-none"
-                            href="#">Your Profile</a><a
-                            className="mt-1 block px-4 py-2 text-base font-medium text-gray-500 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-800 focus:bg-gray-100 focus:text-gray-800 focus:outline-none"
-                            href="#">Settings</a><a
-                            className="mt-1 block px-4 py-2 text-base font-medium text-gray-500 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-800 focus:bg-gray-100 focus:text-gray-800 focus:outline-none"
-                            href="#">Sign out</a></div>
+                        <div className="mt-3">
+                            {settingsItems.map(item =>
+                                <a
+                                    key={item}
+                                    className="mt-1 block px-4 py-2 text-base font-medium text-white"
+                                    href="#">{item}</a>
+                            )}
+
+                        </div>
                     </div>
                 </div>
             </nav>
